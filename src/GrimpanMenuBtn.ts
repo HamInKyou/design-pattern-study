6e7a5c19f8f1912d93f0e327975c08a7d035a516
hig1207 <hig1207@midasin.com>
2025-01-17 21:41:59 +0900

    Revert "[Builder] :: 빌더 패턴 ( 외장 빌더 + Director )"
    This reverts commit ed3624be350d1584a1d89fba4f26974f25969321.

--- src/GrimpanMenuBtn.ts
+++ src/GrimpanMenuBtn.ts
@@ -1,14 +1,14 @@
 class GrimpanMenuBtn {
-  name?: string;
-  type?: string;
-  onClick?: () => void;
-  onChange?: () => void;
-  active?: boolean;
-  value?: string | number;
+  private name: string;
+  private type: string;
+  private onClick?: () => void;
+  private onChange?: () => void;
+  private active?: boolean;
+  private value?: string | number;
 
-  constructor(
-    name?: string,
-    type?: string,
+  private constructor(
+    name: string,
+    type: string,
     onClick?: () => void,
     onChange?: () => void,
     active?: boolean,
@@ -21,120 +21,51 @@ class GrimpanMenuBtn {
     this.active = active;
     this.value = value;
   }
+
+  static Builder = class GrimpanMenuBtnBuilder {
+    btn: GrimpanMenuBtn;
+
+    constructor(name: string, type: string) {
+      this.btn = new GrimpanMenuBtn(name, type);
+    }
+
+    setName(name: string) {
+      this.btn.name = name;
+      return this;
+    }
+
+    setType(type: string) {
+      this.btn.type = type;
+      return this;
+    }
+
+    setOnClick(onClick: () => void) {
+      this.btn.onClick = onClick;
+      return this;
+    }
+
+    setOnChange(onChange: () => void) {
+      this.btn.onChange = onChange;
+      return this;
+    }
+
+    setActive(active: boolean) {
+      this.btn.active = active;
+      return this;
+    }
+
+    setValue(value: string | number) {
+      this.btn.value = value;
+      return this;
+    }
+
+    build() {
+      return this.btn;
+    }
+  };
 }
 
-interface GrimpanMenuBtnBuilder {
-  setName(name: string): this;
-  setType(type: string): this;
-  setOnClick(onClick: () => void): this;
-  setOnChange(onChange: () => void): this;
-  setActive(active: boolean): this;
-  setValue(value: string | number): this;
-  build(): GrimpanMenuBtn;
-}
-
-class ChromeGrimpanMenuBtnBuilder implements GrimpanMenuBtnBuilder {
-  btn: GrimpanMenuBtn;
-
-  constructor() {
-    this.btn = new GrimpanMenuBtn();
-  }
-
-  setName(name: string) {
-    this.btn.name = name;
-    return this;
-  }
-
-  setType(type: string) {
-    this.btn.type = type;
-    return this;
-  }
-
-  setOnClick(onClick: () => void) {
-    this.btn.onClick = onClick;
-    return this;
-  }
-
-  setOnChange(onChange: () => void) {
-    this.btn.onChange = onChange;
-    return this;
-  }
-
-  setActive(active: boolean) {
-    this.btn.active = active;
-    return this;
-  }
-
-  setValue(value: string | number) {
-    this.btn.value = value;
-    return this;
-  }
-
-  build() {
-    return this.btn;
-  }
-}
-
-class IEGrimpanMenuBtnBuilder implements GrimpanMenuBtnBuilder {
-  btn: GrimpanMenuBtn;
-
-  constructor() {
-    this.btn = new GrimpanMenuBtn();
-  }
-
-  setName(name: string) {
-    this.btn.name = name;
-    return this;
-  }
-
-  setType(type: string) {
-    this.btn.type = type;
-    return this;
-  }
-
-  setOnClick(onClick: () => void) {
-    this.btn.onClick = onClick;
-    return this;
-  }
-
-  setOnChange(onChange: () => void) {
-    this.btn.onChange = onChange;
-    return this;
-  }
-
-  setActive(active: boolean) {
-    this.btn.active = active;
-    return this;
-  }
-
-  setValue(value: string | number) {
-    this.btn.value = value;
-    return this;
-  }
-
-  build() {
-    return this.btn;
-  }
-}
-
-export class GrimpanMenuBtnDirector {
-  static createBackBtn(builder: GrimpanMenuBtnBuilder) {
-    const baclBtnBuilder = builder
-      .setName('뒤로')
-      .setType('back')
-      .setOnChange(() => {})
-      .setActive(false);
-    return baclBtnBuilder;
-  }
-  static createForwardBtn(builder: GrimpanMenuBtnBuilder) {
-    const forwardBtnBuilder = builder
-      .setName('앞으로')
-      .setType('forward')
-      .setOnChange(() => {})
-      .setActive(false);
-    return forwardBtnBuilder;
-  }
-}
-
-GrimpanMenuBtnDirector.createBackBtn(new ChromeGrimpanMenuBtnBuilder());
-GrimpanMenuBtnDirector.createForwardBtn(new IEGrimpanMenuBtnBuilder());
+const backBtn = new GrimpanMenuBtn.Builder('뒤로', 'back')
+  .setOnClick(() => {})
+  .setActive(false)
+  .build();
